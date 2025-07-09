58c943ebb0d8f235a20a763b7d818dae87b58af0
admin.hatinh <admin.hatinh@vss.local>
2025-07-09 10:24:41 +0700

    commit

--- config.company.js
+++ config.company.js
@@ -4,10 +4,10 @@ export default {
     companyNameThuong: "Công ty TNHH Tư vấn và Dịch vụ An sinh Hồng Lam",
     bhxhName: "BẢO HIỂM XÃ HỘI HUYỆN THẠCH HÀ",
     apiBaseURL: "http://14.224.129.177:4209", // CỔNG 4209
-    clientURL: "http://14.224.129.177:1979", // CLIENT
-    port_client: 1979,
+    clientURL: "http://14.224.129.177", // CLIENT
+    port_client: 80,
     port_backend: 4209, 
     host: '192.168.1.97', // MÁY CHỦ TUANPDA
     namebienlai: 'ansinhhonglam',
-    urlBienlaidientu: `http://14.224.129.177:1979/tracuubienlaidientu-ansinhhonglam`
+    urlBienlaidientu: `http://14.224.129.177/tracuubienlaidientu-ansinhhonglam`
 };
