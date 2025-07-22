9adfc49eb9f0d89d382e07c0664ef38e2bede80a
admin.hatinh <admin.hatinh@vss.local>
2025-07-22 09:28:47 +0700

    commit

--- pages/nhanviendailythu/tracuuhanthe.vue
+++ pages/nhanviendailythu/tracuuhanthe.vue
@@ -90,7 +90,7 @@
               <td style="text-align: center">{{ item.ngaysinh }}</td>
               <td style="text-align: center">{{ item.sodienthoai }}</td>
               <td style="text-align: center">
-                {{ item.gioitinh === 1 ? "Nữ" : "Nam" }}
+                {{ item.gioitinh === "1" ? "Nam" : "Nữ" }}
               </td>
               <td style="text-align: center">
                 {{ loaihinh === "bhyt" ? item.hanthecu : item.tuthang }}
